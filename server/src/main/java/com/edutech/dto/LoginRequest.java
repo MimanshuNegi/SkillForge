629788f43e119d7b1a845973de795a78f51376f6
MimanshuNegi <dummy@gmail.com>
2026-05-13 04:10:56 +0000

    Entity and DTO created

--- server/src/main/java/com/edutech/dto/LoginRequest.java
+++ server/src/main/java/com/edutech/dto/LoginRequest.java
@@ -1,17 +1,23 @@
 package com.edutech.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LoginRequest {
 
+    @NotBlank
     private final String username;
+
+    @NotBlank
     private final String password;
 
     @JsonCreator
     public LoginRequest(
         @JsonProperty("username") String username,
         @JsonProperty("password") String password) {
+
         this.username = username;
         this.password = password;
     }
